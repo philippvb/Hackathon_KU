8b4161c171b95a46ae0c17d5a1130f67170ae044
philippvb <philippvonbachmann@web.de>
2021-04-10 19:09:37 +0200

    Added environment description

---
+++ src/env.py
@@ -0,0 +1,27 @@
+
+
+class VaccinationEnviroment:
+    def __init__(self, config):
+        # add all different groups
+        pass
+        # add vacin shipping schedule
+
+    def get_vaccines(self):
+        pass
+    # returns next vaccine objects
+
+    def step(self, vaccines):
+        pass
+    # vaccines
+
+    def get_info(self):
+        pass
+    # returns general information like current rates for group ...
+
+    def plot(self):
+        pass
+    # plots current status
+
+    def save(self):
+        pass
+        # saves history
